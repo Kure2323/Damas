30e716b5c42c66088f558c1e597a4f637c9f3639
Kure2323 <polgorrindo@gmail.com>
2024-12-06 19:42:03 +0100

    Archivo de 'Reglas' y métodos de print de matrices

--- LasDamas/src/main/java/org/example/Method.java
+++ LasDamas/src/main/java/org/example/Method.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class Method {
+    public static void MostrarMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }   //Muestra por pantalla la matriz introducida
+
+    public static void MostrarMatriz(String[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }   //Muestra por pantalla la matriz introducida
+
+
+
+
+
+
+
 }
