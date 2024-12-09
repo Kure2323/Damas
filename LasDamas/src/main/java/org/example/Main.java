8bf30279ff04b887b55b8a6f4cdbc0f1886e6332
A718\1dam <polgorrindo@gmail.com>
2024-12-09 20:43:56 +0100

    Method, matriz inversa a cambio de matriz

--- LasDamas/src/main/java/org/example/Main.java
+++ LasDamas/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Cara de Ass");
+
+        int pos = 0;
+
+        String[][] matriz = new String[3][3];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                matriz[i][j] = Integer.toString(pos);
+                pos++;
+            }
+        }
+        Method.MostrarMatriz(matriz);
+        System.out.println();
+        matriz = Method.InvertirMatriz(matriz);
+        System.out.println();
+        Method.MostrarMatriz(matriz);
+
+
+
     }
 }
--- LasDamas/src/main/java/org/example/Method.java
+++ LasDamas/src/main/java/org/example/Method.java
@@ -19,7 +19,26 @@ public class Method {
         }
     }   //Muestra por pantalla la matriz introducida
 
+    public static String[][] InvertirMatriz(String[][] matriz) {
 
+        String[] contenido = new String[matriz.length * matriz[0].length];
+        int pos = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                contenido[pos] = matriz[i][j];
+                pos++;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                pos--;
+                matriz[i][j] = contenido[pos];
+            }
+        }
+        return matriz;
+    }
 
 
 
