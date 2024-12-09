d82f25eb1231c65f6b715c08616411b2aa64ae42
PC_IZAN\Izan López <izanlopez1010@gmail.com>
2024-12-09 20:50:13 +0100

    no message

--- LasDamas/src/main/java/org/example/Method.java
+++ LasDamas/src/main/java/org/example/Method.java
@@ -38,8 +38,17 @@ public class Method {
             }
         }
         return matriz;
-    }
+    } //Invierte y sobrescribe una matriz
 
+    public static void MostrarMatrizInversa(String[][] matriz) {
+
+        for (int i = matriz.length -1 ; i >= 0; i--) {
+            for (int j = matriz[i].length -1; j >=0; j--) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }   //Muestra por pantalla la matriz introducida de forma inversa
 
 
 
