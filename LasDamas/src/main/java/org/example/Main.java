318f6e394e22dfce953e37d624162c718fb05d8a
Pol <polgorrindo@gmail.com>
2025-01-02 22:53:30 +0100

    Lavado de cara y dejada de tonterías.
    He vuelto a cambiar las fichas y el tablero, ahora es mucho más visual, además es mucho más entendible.
    También he dejado la tontería de darle la vuelta al tablero y mi paranoia esa.
    *Función fix: cambioDama ahora funciona con los caracteres, ya no hace toUppercase.
    *Se eliminan las funciones: invertirMatriz y mostrarMatrizInversa pues ya no tienen ningún sentido tenerlas.
    *Creación de la función ejecutarMovimiento: función que he creado con la intención de que redirija a otras funciones para que ejecuten el movimiento teniendo en cuenta qué ficha es la que queremos mover.
    Ah, y ahora el boolean turno no es static

--- LasDamas/src/main/java/org/example/Main.java
+++ LasDamas/src/main/java/org/example/Main.java
@@ -5,27 +5,29 @@ public class Main {
 
     static Scanner entrada = new Scanner(System.in);
 
-    static boolean turno = true;    //Cuando sea true = turno de los blancos / Cuando sea false = turno de los negros
-    //Este boolean lleva la dependencia del turno, tanto de los blancos como de los negros, la inversa de la matriz depende del turno también
-
-    static String[][] tablero = {{null,"n",null,"n",null,"n",null,"n"}, {"n",null,"n",null,"n",null,"n",null}, {null,"n",null,"n",null,"n",null,"n"}, {null,null,null,null,null,null,null,null}, {null,null,null,null,null,null,null,null}, {"b",null,"b",null,"b",null,"b",null}, {null,"b",null,"b",null,"b",null,"b"}, {"b",null,"b",null,"b",null,"b",null}};
+    static String[][] tablero = {{null,"○",null,"○",null,"○",null,"○"}, {"○",null,"○",null,"○",null,"○",null}, {null,"○",null,"○",null,"○",null,"○"}, {null,null,null,null,null,null,null,null}, {null,null,null,null,null,null,null,null}, {"●",null,"●",null,"●",null,"●",null}, {null,"●",null,"●",null,"●",null,"●"}, {"●",null,"●",null,"●",null,"●",null}};
     //Matriz del tablero con únicamente las fichas, este sería el tablero a modificar durante toda la partida
     //Para mostrarla por pantalla primero deberíamos pasarla por un método que rellene los 'null' con las casillas blancas y negras
 
     public static void main(String[] args) {
-
+        boolean turno = true;    //Cuando sea true = turno de los blancos / Cuando sea false = turno de los negros
+        //Este boolean lleva la dependencia del turno, tanto de los blancos como de los negros, la inversa de la matriz depende del turno también
+        String color;
         Method.plantillaInicio();
         while (true) {
+
             if (turno) {
-                String color = "b";
+                color = "●";
             } else {
-                String color = "n";
+                color = "○";
             }
-            String[] movimiento = Method.pedirMovimiento();
-            Method.mostrarTablero(tablero);
+            Method.mostrarTablero();
+            String[] movimiento = Method.pedirMovimiento(turno);
+            Method.limpiarPantalla();
+            Method.cambioDama(turno);
+            System.out.println();
 
-
-            break;
+            turno = !turno;
         }
 
 
--- LasDamas/src/main/java/org/example/Method.java
+++ LasDamas/src/main/java/org/example/Method.java
@@ -2,18 +2,24 @@ package org.example;
 
 import java.util.Scanner;
 
-import static org.example.Main.turno;
+import static org.example.Main.tablero;
 
 public class Method {
 
     static Scanner entrada = new Scanner(System.in);
 
+    /**
+     * Limpia la pantalla, es decir, desplaza toda la información hacia arriba dejando la terminal en blanco.
+     */
     public static void limpiarPantalla() {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
-    } //"Limpiar" la pantalla
+    }
 
+    /**
+     * Plantilla que muestra la bienvenida y lo básico para saber de que va el programa, las reglas.
+     */
     public static void plantillaInicio() {
 
         boolean salir = false;
@@ -26,15 +32,15 @@ public class Method {
             System.out.println("Tablero de 8x8 (64) dividido en 2 colores: blanco (#) y negro ( )");
             System.out.println("Peones: Avanzan y capturan en diagonal sólo hacia adelante y sólo en avance de una casilla.");
             System.out.println("Damas: Tanto para desplazarse como para realizar capturas, avanzan el número de casillas que se desee tanto hacia delante como hacia atrás, siempre en diagonal.");
-            System.out.println("Peones blancos: b");
-            System.out.println("Damas blancas: B");
-            System.out.println("Peones negros: n");
-            System.out.println("Damas negras: N");
+            System.out.println("Peones blancos: ●");
+            System.out.println("Damas blancas: ♛");
+            System.out.println("Peones negros: ○");
+            System.out.println("Damas negras: ♕");
             System.out.println("Si consigues que un peón llegue a la parte opuesta de tu tablero este se convertirá en una dama");
             System.out.println("¿Empezamos?");
             System.out.println("(S para empezar, N u otra cosa para repetir las reglas)");
             String empezar = entrada.next();
-            if (empezar.equals("S")) {
+            if (empezar.equals("S") || empezar.equals("s")) {
                 salir=true;
             } else {
                 limpiarPantalla();
@@ -43,7 +49,7 @@ public class Method {
 
         } while (!salir);
 
-    } // Muestra las reglas del juego
+    }
 
     public static void mostrarMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
@@ -63,55 +69,36 @@ public class Method {
         }
     }   //Muestra por pantalla la matriz introducida
 
-    public static String[][] invertirMatriz(String[][] matriz) {
-
-        String[] contenido = new String[matriz.length * matriz[0].length];
-        int pos = 0;
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                contenido[pos] = matriz[i][j];
-                pos++;
+    /**
+     * Cambia a DAMAS los peones situados en el extremo contrario al que pertenecen.
+     * @param tablero
+     * @param color
+     * @return Tablero con las Damas
+     */
+    public static String[][] cambioDama(boolean turno) {  //Comprueba y cambia los peones que estén situados al extremo contrario a damas
+        if (turno) {
+            for (int i = 0; i < tablero.length; i++) {
+                if (tablero[0][i].equals("●")) {
+                    tablero[0][i] = "♛";
+                }
+            }
+        } else {
+            for (int i = 0; i < tablero.length; i++) {
+                if (tablero[7][i].equals("○")) {
+                    tablero[7][i] = "♕";
+                }
             }
         }
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                pos--;
-                matriz[i][j] = contenido[pos];
-            }
-        }
-        return matriz;
-    } //Invierte y sobrescribe una matriz
-
-    public static void mostrarMatrizInversa(String[][] matriz) {
-
-        for (int i = matriz.length -1 ; i >= 0; i--) {
-            for (int j = matriz[i].length -1; j >=0; j--) {
-                System.out.print(matriz[i][j] + "  ");
-            }
-            System.out.println();
-        }
-    }   //Muestra por pantalla la matriz introducida de forma inversa
-
-    public static String[][] cambioDama(String[][] matriz, String color) {  //Comprueba y cambia los peones que estén situados al extremo contrario a damas
-        for (int i = 0; i < matriz[0].length; i++) {
-            if (matriz[0][i] == color) {
-                matriz[0][i] = color.toUpperCase();
-            }
-        }
-        return matriz;
+        return tablero;
     }   //Si hay un peón en el extremo contrario lo cambia a dama
 
-    public static String[] pedirMovimiento() {
+    public static String[] pedirMovimiento(boolean turno) {
 
         String team;
         if (turno) {
             team = "Blancas";
-            String color = "b";
         } else {
             team = "Negras";
-            String color = "n";
         }
         String[] vector;
         while (true) {
@@ -119,8 +106,8 @@ public class Method {
                 System.out.println("Es turno de las " + team + ", introduzca primero la posición de la ficha a mover, un espacio y la posición que quieres que vaya.");
                 Scanner entrada = new Scanner(System.in);
                 String aux = entrada.nextLine();
-                if (!aux.matches("[0-8][0-8] [0-8][0-8]")) {
-                    System.out.println("Debe introducirse los datos de la siguiente manera: [0-8][0-8] [0-8][0-8]");
+                if (!aux.matches("[0-7][0-7] [0-7][0-7]")) {
+                    System.out.println("Debe introducirse los datos de la siguiente manera: [0-7][0-7] [0-7][0-7]");
                     continue;
                 }
                 vector = aux.split(" ");
@@ -138,13 +125,13 @@ public class Method {
 
     }   //Pide la jugada y comprueba su formato
 
-    public static void mostrarTablero(String[][] tablero) {
+    public static void mostrarTablero() {
 
-        String[][] casillas = {{" ", "#"," ", "#"," ", "#"," ", "#"},{"#"," ", "#"," ", "#"," ", "#"," "},
-                {" ", "#"," ", "#"," ", "#"," ", "#"},{"#"," ", "#"," ", "#"," ", "#"," "},
-                {" ", "#"," ", "#"," ", "#"," ", "#"},{"#"," ", "#"," ", "#"," ", "#"," "},
-                {" ", "#"," ", "#"," ", "#"," ", "#"},{"#"," ", "#"," ", "#"," ", "#"," "}};
-
+        String[][] casillas = {{" ", "."," ", "."," ", "."," ", "."},{"."," ", "."," ", "."," ", "."," "},
+                {" ", "."," ", "."," ", "."," ", "."},{"."," ", "."," ", "."," ", "."," "},
+                {" ", "."," ", "."," ", "."," ", "."},{"."," ", "."," ", "."," ", "."," "},
+                {" ", "."," ", "."," ", "."," ", "."},{"."," ", "."," ", "."," ", "."," "}};
+        System.out.println("_______________________");
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
                 if (tablero[i][j] == null) {
@@ -154,11 +141,40 @@ public class Method {
             }
             System.out.println();
         }
+        System.out.println("_______________________");
 
     }   //Junta las matrices de tablero y le pone las casillas para mostrarlo por pantalla
 
+    /**
+     * Función para ejecutar el movimiento introducido por el usuario.
+     * Primero debe haber pasado por controles para que no pueda saltar errores.
+     * Controles tales como: Comprobar que la ficha a mover existe y que a donde vaya no haya otra ficha de su mismo color o que tipo de ficha es...
+     * @param movimiento
+     * @param turno
+     * @return
+     */
+    public static String[][] ejecutarMovimiento(String movimiento, boolean turno) {
 
+        if (turno) {
+            if (tablero[Integer.parseInt(movimiento.substring(0, 1))][Integer.parseInt(movimiento.substring(1, 2))].equals("●")) {
+                //Enviar a método de ejecución de movimiento de peón.
+            } else if (tablero[Integer.parseInt(movimiento.substring(0, 1))][Integer.parseInt(movimiento.substring(1, 2))].equals("♛")) {
+                //Enviar a método de ejecución de movimiento de DAMA.
+            } else {
+                //Enviar a método que avise del error y vuelva pedir el movimiento.
+            }
+        } else {
+            if (tablero[Integer.parseInt(movimiento.substring(0, 1))][Integer.parseInt(movimiento.substring(1, 2))].equals("○")) {
+                //Enviar a método de ejecución de movimiento de peón.
+            } else if (tablero[Integer.parseInt(movimiento.substring(0, 1))][Integer.parseInt(movimiento.substring(1, 2))].equals("♕")) {
+                //Enviar a método de ejecución de movimiento de DAMA.
+            } else {
+                //Enviar a método que avise del error y vuelva pedir el movimiento.
+            }
+        }
 
+        return tablero;
+    }
 
 
 }
